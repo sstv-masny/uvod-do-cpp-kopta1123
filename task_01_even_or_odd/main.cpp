24cf00343669908e92a2671c8f374a93428e85b6
kopta1123 <timotej.kopta@gmail.com>
2026-06-01 22:16:30 +0200

    Update main.cpp

--- task_01_even_or_odd/main.cpp
+++ task_01_even_or_odd/main.cpp
@@ -4,11 +4,11 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     long long n;
-    if (!(std::cin >> n)) return 0;
-{    cout << "even";
-}
-    else{
- std::cout << "odd";
+   if (!(std::cin >> n)) return 0;
+    if (n % 2 == 0) {
+        std::cout << "even";
+    } else {
+        std::cout << "odd";
     }
     return 0;
 }
